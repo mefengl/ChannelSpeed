395ca6528b0e9ffbfc262a9eeff3aeaed3eec80e
不游 <71683364+mefengl@users.noreply.github.com>
2024-06-03 21:00:50 +0800

    Initial commit

---
+++ wxt.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'wxt'
+import react from '@vitejs/plugin-react'
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  manifest: {
+    permissions: [],
+  },
+  srcDir: 'src',
+  vite: () => ({
+    plugins: [react()],
+  }),
+})
